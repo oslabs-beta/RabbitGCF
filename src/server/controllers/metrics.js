2ec2f7b678df8fa5d3eef46b42c833e8cb7acda9
dpark001 <dparkzero1@gmail.com>
2024-06-11 19:41:45 -0700

    more debugging for metrics, logs, auth

--- src/server/controllers/metrics.js
+++ src/server/controllers/metrics.js
@@ -1,7 +1,17 @@
 const monitoring = require('@google-cloud/monitoring');
+const cloudFuncs = require('@google-cloud/functions');
+const { Logging } = require('@google-cloud/logging');
+const fs = require('fs');
+const keyFile = require('../../../refined-engine-424416-p7-644c42d9ba48.json');
 
 // create a client
-const client = new monitoring.MetricServiceClient();
+// const client = new monitoring.MetricServiceClient();
+// const connection = async () => {
+//   await client.initialize();
+// }
+// connection();
+
+
 const metrics = [
   'metric.type="cloudfunctions.googleapis.com/function/execution_count"',
   'metric.type="cloudfunctions.googleapis.com/function/execution_times"',
@@ -11,6 +21,8 @@ const metrics = [
 
 const metricsController = {
   getMetrics: async (req, res, next) => {
+    // cloudFuncs.v2.
+    
     const { projectId } = req.params;
     console.log(`getMetrics projectId: ${projectId}`);
 
@@ -29,12 +41,13 @@ const metricsController = {
         }
       };
     });
-    console.log(`getMetrics request objects: ${requests}`);
+    console.log(`getMetrics request objects: ${typeof requests}`);
+    console.log(`getMetrics request object at index 0: ${requests[0]}`);
 
     try {
       const responses = requests.map(async (el, index) => {
         console.log(`This is requests[el]: ${requests[index]}`);
-        const apiCall = await client.listTimeSeries(requests[index]);
+        const [apiCall] = await client.listTimeSeries(requests[index]);
         console.log(`listTimeSeries response for ${el}: ${apiCall}`);
         return apiCall;
       });
@@ -43,8 +56,20 @@ const metricsController = {
     } catch (err) {
       return next(`Something went wrong. ERROR: ${err}`);
     }
+  },
+
+  test: async (req, res, next) => {
+    const projectId = 'refined-engine-424416-p7';
+
+    const login = new Logging({ credentials: keyFile });
+    const logName = 'cloudfunctions.googleapis.com%2Fcloud-functions';
+
+    const newLogs = await login.getEntries();
+    // console.log(`This is newLogs: ${newLogs}`);
   }
 };
+
+metricsController.test();
 
 
 module.exports = metricsController;
