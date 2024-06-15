e214158ebff22d1a9fe4a793bf7bf17170aa8e92
Wilson7chen <153645810+Wilson7chen@users.noreply.github.com>
2024-06-14 22:14:05 -0400

    Update User.js

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,43 +1,43 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// // connect to mongoDB
-// const myURI = process.env.MONGO_URI;
-// const connectDB = async () => {
-//   const est = await mongoose.connect(myURI);
-//   console.log(`MongoDB connected: ${est.connection.host}`);
-// };
-// connectDB();
+// connect to mongoDB
+const myURI = process.env.MONGO_URI;
+const connectDB = async () => {
+  const est = await mongoose.connect(myURI);
+  console.log(`MongoDB connected: ${est.connection.host}`);
+};
+connectDB();
 
-// // define userSchema
-// const userSchema = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     required: true
-//   },
-//   lastName: {
-//     type: String,
-//     // required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   id: {
-//     type: String,
-//     required: true
-//   },
-//   picture: {
-//     type: String,
-//     required: true
-//   }
-// },
-// {
-//   timestamps: true
-// });
+// define userSchema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    // required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: true
+});
 
-// // define user model
-// const User = mongoose.model('User', userSchema);
+// define user model
+const User = mongoose.model('User', userSchema);
 
-// // export user model
-// module.exports = User;
+// export user model
+module.exports = User;
