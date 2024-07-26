6699425b88837902f419e732e69fca2809370519
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-07-25 19:54:02 -0700

    gcloud service account auth included

--- src/server/controllers/metrics.js
+++ src/server/controllers/metrics.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const monitoring = require('@google-cloud/monitoring');
 const { FunctionServiceClient } = require('@google-cloud/functions').v2;
 
+const keyFilename = path.join(__dirname,'../../../refined-engine-424416-p7-60ddb1f75e87.json');
+
 // create needed clients
-const funcsClient = new FunctionServiceClient();
-const monClient = new monitoring.MetricServiceClient();
+const funcsClient = new FunctionServiceClient({ keyFilename });
+const monClient = new monitoring.MetricServiceClient({ keyFilename });
 
 
 // const metrics = [
