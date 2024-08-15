65d7c27a5cfdb025db1a85bcb05d5cf20806e5a2
gitbrendanlam <141107696+gitbrendanlam@users.noreply.github.com>
2024-08-14 17:21:57 -0700

    created oauth front end

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,10 +1,22 @@
 // Package dependencies
 const express = require('express');
 const router = express.Router();
+const { createUser, getUser } = require('./../controllers/userController.js')
 
 // Google OAuth, step 1: Redirect to Google OAuth consent screen
-router.get('/info', (req, res) => {
+// router.get('/info', (req, res) => {
   
+// });
+
+router.post('/login', (req, res, next) => {
+  console.log('invoked login route'); 
+  return next();
+}, createUser, (req, res) => {
+  res.status(200);
 });
+
+// router.post('/create', createUser, getUser, (req, res) => {
+
+// });
 
 module.exports = router;
